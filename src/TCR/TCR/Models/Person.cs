7dce6d02fad0b1b3ebd4718fefc8a3cd27a15d21
babarykin_denis <babarykin.denis@gmail.com>
2015-06-12 03:35:16 +0300

    MIGRATION TO ASP MVC FULL STACK PROJECT

---
+++ src/TCR/TCR/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TCR.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<PersonalReceptor> PersonalReceptors { get; set; }
+
+        public Person()
+        {
+            PersonalReceptors = new List<PersonalReceptor>();
+        }
+    }
+}
